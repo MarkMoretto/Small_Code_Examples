ba668e9b9fcce928c4ca0bb000e68baf7f0bd833
Mark Moretto <otteromkram@gmail.com>
2019-09-18 09:35:07 -0400

    Added dialog
    Additional dialog helps to explain the process.

--- process-zipfile-iteratively-with-temp-folder.py
+++ process-zipfile-iteratively-with-temp-folder.py
@@ -12,15 +12,23 @@ import zipfile
 import tempfile
 import pandas as pd
 
+### Replace with path to zipfile, including extension
 full_zip_path = r'C:\path\to\zipfile.zip'
 
 def process_zipfile():
+    ### Using try-except in the event that a file doesn't exist.
     try:
+        ### Using nested with statements to keep temp folder open
+        ### while each file processs.
         with tempfile.TemporaryDirectory() as temp_dir:
             with zipfile.ZipFile(full_zip_path, 'r') as zf:
                 file_list = [i.filename for i in zf.infolist()]
                 zf.extractall(temp_dir)
+                
+            ### Filename is static, but could be formatted based on zipfile contents.
             filename = 'my-file-name.txt'
+            
+            ### Dataframe isn't necessary; process a file however you'd like.
             df = pd.read_csv(path.join(temp_dir, filename), low_memory = False)
         try:
             zf.close()
@@ -30,6 +38,7 @@ def process_zipfile():
         return df
 
     except FileNotFoundError:
+        ### Print out a bit of info if an error is raised.
         tmp_name = full_zip_path.split('\\')[-1]
         print(f'Could not find File in zipfile {tmp_name}')
         pass
